85c18ff6433a9476f80a25134db506f61ed36e39
RISHABH TRIPATHI <53142046+rishabhtripathi123@users.noreply.github.com>
2019-11-09 21:05:11 +0530

    Add files via upload
    It's the GUi ,which predicts and tell whether the respective website is phishing or not

---
+++ phishing.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import sqlite3 as sql
+scr=Tk()
+scr.geometry('1200x800+0+0')
+l=Label(scr,text="Phishing Website Detection",font=('times',20,'bold'),bg='blue',fg='yellow')
+l.pack(side=TOP,fill=X)
+f=Frame(scr,bg='yellow')
+f.pack(fill=BOTH,expand=12)
+v=StringVar()
+l1=Label(f,text='Write The Website',font=('times',20,'bold'),bg='blue',fg='yellow')
+l1.place(x=200,y=100)
+l2=Label(f,text='Phishing / Not Phishing',font=('times',20,'bold'),bg='blue',fg='yellow')
+l2.place(x=200,y=300)
+us=Entry(f,font=('times',20,'bold'),bg='blue',fg='yellow',textvariable=v)
+us.place(x=500,y=100)
+ps=Entry(f,font=('times',20,'bold'),bg='blue',fg='yellow')
+ps.place(x=500,y=300)
+b1=Button(f,text='Predict',font=('times',20,'bold'),command=lambda n:v.get())
+b1.place(x=400,y=200)
+scr.mainloop()
+
